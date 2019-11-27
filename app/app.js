1e2b7653732ed093b0ab2f44a6c6692b25946d37
Malathi Thangarajan <malathi.thangarajan@freshworks.com>
2019-11-27 13:40:56 +0530

    test commit

--- app/app.js
+++ app/app.js
@@ -11,6 +11,24 @@ $(document).ready( function() {
                     .catch(function(e) {
                         console.log('Exception - ', e);
                     });
+                    console.log("going to call the list tasks api.");
+                    var self = this,
+                        headers = { Authorization: "bearer <%= access_token %>"},
+                        reqData = { headers: headers, isOAuth: true },
+                        url = "https://www.googleapis.com/tasks/v1/users/@me/lists?key=AIzaSyBtXBBgIIUHqeDz_dcui7p8Iroujhq5OAI";
+                    console.log("going to call the list tasks api. One step ahead.");
+                    client.request.get(url, reqData).then(
+                        function(data) {
+                           console.log("tasks lists: ", data);
+                           // var response = JSON.parse(data.response)["value"];
+                           // handleSuccess(response);
+                        },
+                        function(error) {
+                          console.log("error:", error);
+                          console.log("error status:", error.status);
+                          //handleError(error);
+                        }
+                    );
         });
     });
 });
